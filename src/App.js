bcafa3c01a66902761a24da4cbd416321bb1d54a
Mehedi Hassan <mehedi.all.info@gmail.com>
2022-08-06 16:03:07 +0600

    HTML Template Integrated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <h1>React Router </h1>
     </div>
   );
