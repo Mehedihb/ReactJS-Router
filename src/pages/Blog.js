c3d1dc4ef10d0e4fd7228c0d73000156b04bb701
Mehedi Hassan <mehedi.all.info@gmail.com>
2022-08-10 17:13:56 +0600

    Card Updated

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -11,17 +11,24 @@ const Blog = () => {
     <>
       <Navbar />
       <h3>This is Blog Page 2</h3>
-      <section>
-        {Blog.map((singleBlog) => {
-          const { id, author, title, description } = singleBlog;
-          return (
-            <article key={id}>
-              <Link to={`/blog/${id}`}>{id}</Link>
-              <h5>{author}</h5>
-              <h6>{title}</h6>
-            </article>
-          )
-        })}
+      <section className='container-fluid h-100'>
+        <div className='row h-100'>
+          <div className='col-3 overflow-auto h-100'>
+            {Blog.map((singleBlog) => {
+              const { id, author, title, description } = singleBlog;
+              return (
+                <div className="card">
+                  <div className="card-body" key={id}>
+                    <h5 className="card-title">{author}</h5>
+                    <p className="card-text">{title}</p>
+                    <Link to={`/blog/${id}`} className="btn btn-primary">{id}</Link>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+
+        </div>
       </section>
 
 
