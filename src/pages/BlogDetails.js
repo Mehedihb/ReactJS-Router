6e41be88d6e570845cc99e0ec354832ca31e2a33
Mehedi Hassan <mehedi.all.info@gmail.com>
2022-08-11 16:37:49 +0600

    issue fixed route

--- src/pages/BlogDetails.js
+++ src/pages/BlogDetails.js
@@ -5,11 +5,18 @@ import { useParams } from 'react-router-dom'
 
 const BlogDetails = () => {
     const {id}= useParams()
-    
+    const [bodyData, setBodyData] = useState("")
+    useEffect (()=>{
+      const blogD = BlogData.filter((singleBlog)=>singleBlog.id === parseInt(id))
+      setBodyData(blogD[0])
+    },[id])
+    // console.log(bodyData.author)
   return (
     <>
     <Navbar/>
     <h1>{id}</h1>
+    <h5>{bodyData.author} - {bodyData.title}</h5>
+    <p>{bodyData.description}</p>
     </>
   )
 }
